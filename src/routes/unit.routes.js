6976cf17a97a552069d86bdbc72819788ba73696
thuyamyint <thuyamyint2022@gmail.com>
2025-09-29 15:07:20 +0900

    cc

--- src/routes/unit.routes.js
+++ src/routes/unit.routes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router();
 const { createUnit, updateUnit, getAllUnit, deleteUnit } = require("../controllers/unit.controller")
-
+const verifyToken = require("../helper/authJwt")
 router.post("/", createUnit)
 router.put("/:id", updateUnit)
 router.get("/", getAllUnit)
